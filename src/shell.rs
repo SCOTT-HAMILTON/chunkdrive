6d8468ff9cf37267e002f3b558fb616ac11f6ac5
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2024-01-07 21:49:08 +0100

    fmt

--- src/shell.rs
+++ src/shell.rs
@@ -99,7 +99,8 @@ pub fn shell(global: Arc<BlockingGlobal>) {
             }
         );
 
-        let line = match context.read_line(liner::Prompt::from(&prompt), None, &mut ShellCompleter) {
+        let line = match context.read_line(liner::Prompt::from(&prompt), None, &mut ShellCompleter)
+        {
             Ok(line) => line,
             Err(_) => break,
         };
